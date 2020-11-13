b0405d5be24c3220051aa6a5f4d331e72ff20d92
JPNYKW <ilikegamesme@gmail.com>
2020-11-14 04:05:06 +0900

    :construction: Implement convert and tokenize

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,40 @@ fn main() {
     let mut file = File::open(filename).expect("File not found");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Reading the file contents");
+
+    let result = convert(contents);
+    println!("result:\n {}", result.expect("Unwrap result"));
+}
+
+fn convert(code: String) -> Result<String, ()> {
+    let mut tokens = code.split_whitespace();//.iter();
+    let mut code = String::new();
+    let mut indents = 0;
+    // println!("tok {:?}", tokens.next());
+
+    loop {
+        let tok = tokens.next(); // token を進める
+        match tok {
+            Some(tok) => {
+                println!("tok {}", tok);
+                match tok {
+                    "class" => {
+                        let name = tokens.next();
+                        code = format!("{}\nconst {} = (", code, name.expect("Unwrap component name"));
+                        tokens.next(); // extends
+                        let extend = tokens.next(); // 継承元
+                        // TODO: 継承元のプロパティを捜査する?
+                        // TODO: props の生成に必要?
+                        code = format!("{}) => {}", code, "{");
+                        indents = indents + 1;
+                    },
+                    _ => {},
+                };
+            },
+            None => break,
+        };
+    }
+
+    // Ok(String::new())
+    Ok(code)
 }
